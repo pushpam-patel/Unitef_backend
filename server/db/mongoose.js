9cd2a6ffdcc2430c9acafd21740813ea1541d62b
pushpam patel <30745430+pushpam-patel@users.noreply.github.com>
2019-03-13 15:59:30 +0530

    some

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://pvp_unite:pushpam0403@ds135255.mlab.com:35255/unite' || 'mongodb://localhost:27017/Unite')
+mongoose.connect('mongodb://pushpam:pushpam1009@ds135255.mlab.com:35255/unite' || 'mongodb://localhost:27017/Unite')
 
 module.exports={mongoose}
